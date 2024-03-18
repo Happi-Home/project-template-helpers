40342930c09149c10515a50626f0863134d861f5
jwyniaHappi <151471843+jwyniaHappi@users.noreply.github.com>
2024-03-17 21:04:00 -0500

    Update createNodeDevContainer.sh

--- createNodeDevContainer.sh
+++ createNodeDevContainer.sh
@@ -3,7 +3,7 @@ cd .devcontainer
 wget -O devcontainer.json https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/devcontainers/nodeTypescript.devcontainer.json
 echo "======================================"
 echo "======================================"
-echo "Reload VSCode project in the devcontainer with CMD+SHIFT+P (Mac) before continuing!!!!
+echo "Reload VSCode project in the devcontainer with CMD+SHIFT+P (Mac) before continuing!!!!"
 echo "======================================"
 echo "======================================"
 read -p "Press ENTER to ack"
