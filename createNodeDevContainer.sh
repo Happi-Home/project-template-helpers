daea064a71190d03330c642df63eefe254f034ac
jwyniaHappi <151471843+jwyniaHappi@users.noreply.github.com>
2024-03-17 20:47:31 -0500

    Update createNodeDevContainer.sh

--- createNodeDevContainer.sh
+++ createNodeDevContainer.sh
@@ -1,4 +1,3 @@
 mkdir .devcontainer
 cd .devcontainer
-wget https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/devcontainers/nodeTypescript.devcontainer.json?token=GHSAT0AAAAAACNQZNCDDJAOWWHH44L4TJ64ZPXTMHQ
-mv nodeTypescript.devcontainer.json devcontainer.json
+wget https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/devcontainers/nodeTypescript.devcontainer.json -O devcontainer.json
