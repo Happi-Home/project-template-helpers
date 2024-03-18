08d3fc472a453cf757c8de650f1fec40ccd5f2ae
jwyniaHappi <151471843+jwyniaHappi@users.noreply.github.com>
2024-03-17 20:33:16 -0500

    Update installNest.sh

--- installNest.sh
+++ installNest.sh
@@ -3,3 +3,5 @@ mv Node.gitignore .gitignore
 npm i -g @nestjs/cli
 nest new . --package-manager npm --skip-git
 npm i @nestjs/swagger
+wget https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/nest/app.controller.ts?token=GHSAT0AAAAAACNQZNCDN5MSB2DADQODFAXQZPXTE5A -P src
+wget https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/nest/main.ts?token=GHSAT0AAAAAACNQZNCCTHDDAT7TYPXNNPLSZPXTHVQ -P src
