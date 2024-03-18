b99ce5362069a79dc453378752bdcbd33e49d657
jwyniaHappi <151471843+jwyniaHappi@users.noreply.github.com>
2024-03-17 20:38:16 -0500

    Create createNodeDevContainer.sh

---
+++ createNodeDevContainer.sh
@@ -0,0 +1,4 @@
+mkdir .devcontainer
+cd .devcontainer
+wget https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/devcontainers/nodeTypescript.devcontainer.json?token=GHSAT0AAAAAACNQZNCDDJAOWWHH44L4TJ64ZPXTMHQ
+mv nodeTypescript.devcontainer.json devcontainer.json
