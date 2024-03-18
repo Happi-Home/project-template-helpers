98f38dbc877afb2d9af97cecc887cedacb52972d
jwyniaHappi <151471843+jwyniaHappi@users.noreply.github.com>
2024-03-17 20:50:49 -0500

    Update createNodeDevContainer.sh

--- createNodeDevContainer.sh
+++ createNodeDevContainer.sh
@@ -1,3 +1,3 @@
 mkdir .devcontainer
 cd .devcontainer
-wget https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/devcontainers/nodeTypescript.devcontainer.json -O devcontainer.json
+wget -O devcontainer.json https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/devcontainers/nodeTypescript.devcontainer.json
