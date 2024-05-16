58ab584abbdecacf7aea07feab81762a019dffe8
jwyniaHappi <151471843+jwyniaHappi@users.noreply.github.com>
2024-05-16 12:39:12 -0500

    Update installNuxt.sh

--- installNuxt.sh
+++ installNuxt.sh
@@ -5,5 +5,9 @@ mkdir services
 mkdir pages
 mkdir assets
 mkdir assets/css
+wget -O nuxt.config.ts https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/nuxt/nuxt.config.ts
+wget -O tailwind.config.ts https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/nuxt/tailwind.config.ts
+wget -O app.vue https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/nuxt/app.vue
+wget -O layouts/default.vue https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/nuxt/layouts/default.vue
+wget -O assets/css/main.css https://raw.githubusercontent.com/Happi-Home/project-template-helpers/main/nuxt/assets/css/main.css
 git add .
-git commit -m "nuxt init"
